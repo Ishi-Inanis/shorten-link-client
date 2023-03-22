eb02cae98a3eafc8e41e708f2ab98f32d123683a
Ishi Inanis <ishi.inanis@gmail.com>
2023-03-22 14:40:49 +0300

    be the first

---
+++ src/App.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+  import ShortLinkCard from './lib/ShortLinkCard.svelte';
+  import { onMount } from 'svelte';
+
+  export let links = [];
+
+  // onMount(async () => {
+  //   const res = await fetch('http://api.localhost/links');
+  //   links = await res.json();
+  //   links = links.map(link => {
+  //     return {
+  //       id: link.id,
+  //       shortLink: link.short_link,
+  //       link: link.link,
+  //       createdAt: new Date(link.created_at).toLocaleDateString(),
+  //       updatedAt: new Date(link.updated_at).toLocaleDateString(),
+  //       endAt:  new Date(link.end_at).toLocaleDateString(),
+  //       qrcode: link.qrcode
+  //     }
+  //   });
+  // });
+
+  links = [{
+    id: 1,
+    shortLink: 'F3Awd4',
+    link: 'https://ya.ru',
+    createdAt: new Date().toLocaleDateString(),
+    updatedAt: new Date().toLocaleDateString(),
+    endAt: new Date().toLocaleDateString(),
+    qrcode: ''
+  }];
+</script>
+
+<div class="bg-grid-slate-400 mask-image absolute top-0 left-0 w-full h-screen"></div>
+
+<main class="min-h-screen p-6 relative">
+  <div class="z-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 blur-3xl">
+    <div class="flex">
+      <div class="w-48 h-48 rounded-full bg-yellow-300/40 right-0 top-0"></div>
+      <div
+          class="w-96 h-96 rounded-full bg-gradient-to-r from-yellow-300/40 to-red-600/40"></div>
+      <div class="w-48 h-48 rounded-full bg-red-600/40 bottom-0 left-0"></div>
+    </div>
+  </div>
+
+  <h1 class="text-center mb-2 text-5xl font-extrabold">
+    <span
+        class="text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-red-600">41t</span>
+  </h1>
+  <h2 class="text-center mb-6 text-4xl font-extrabold">Shorten links</h2>
+
+  <section class="grid grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] gap-6">
+    {#each links as { id, shortLink, link, createdAt, updatedAt, endAt, qrcode }}
+      <ShortLinkCard {id} {shortLink} {link} {createdAt} {updatedAt} {endAt} {qrcode}/>
+      <ShortLinkCard {id} {shortLink} {link} {createdAt} {updatedAt} {endAt} {qrcode}/>
+      <ShortLinkCard {id} {shortLink} {link} {createdAt} {updatedAt} {endAt} {qrcode}/>
+      <ShortLinkCard {id} {shortLink} {link} {createdAt} {updatedAt} {endAt} {qrcode}/>
+      <ShortLinkCard {id} {shortLink} {link} {createdAt} {updatedAt} {endAt} {qrcode}/>
+      <ShortLinkCard {id} {shortLink} {link} {createdAt} {updatedAt} {endAt} {qrcode}/>
+      <ShortLinkCard {id} {shortLink} {link} {createdAt} {updatedAt} {endAt} {qrcode}/>
+      <ShortLinkCard {id} {shortLink} {link} {createdAt} {updatedAt} {endAt} {qrcode}/>
+      <ShortLinkCard {id} {shortLink} {link} {createdAt} {updatedAt} {endAt} {qrcode}/>
+    {/each}
+  </section>
+</main>
+
+<!--<main class="flex flex-col justify-center items-center min-h-screen">-->
+<!--  <h1 class="text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-red-600">-->
+<!--    41t</h1>-->
+<!--  <form on:submit={submit} name="auth"-->
+<!--           class="text-lg grid shadow-xl m-6 py-6 px-8 gap-6 bg-white/40 dark:bg-neutral-700/40 border border-neutral-50 dark:border-neutral-600 rounded-xl">-->
+<!--    <header>-->
+<!--      <h2 class="text-center text-3xl">Вход</h2>-->
+<!--    </header>-->
+<!--    <label for="login" class="space-y-2">-->
+<!--      <span class="block">Логин</span>-->
+<!--      <input id="login" name="login" placeholder="" required-->
+<!--             class="outline-none duration-100 focus:ring focus:ring-amber-400/40 bg-neutral-100 dark:bg-neutral-800 px-4 py-2 border border-neutral-50 dark:border-neutral-600 focus:shadow-md focus:border-amber-300 hover:border-amber-300 rounded-xl">-->
+<!--    </label>-->
+
+<!--    <label for="password" class="space-y-2">-->
+<!--      <span class="block">Пароль</span>-->
+<!--      <input type="password" id="password" name="password" placeholder="" required-->
+<!--             class="outline-none duration-100 focus:ring focus:ring-amber-400/40 bg-neutral-100 dark:bg-neutral-800 px-4 py-2 border border-neutral-50 dark:border-neutral-600 focus:shadow-md focus:border-amber-300 hover:border-amber-300 rounded-xl">-->
+<!--    </label>-->
+
+<!--    <button-->
+<!--        class="outline-none duration-100 text-neutral-900 justify-self-end px-4 py-2 bg-amber-400/80 shadow-md hover:shadow-amber-400 focus:ring focus:ring-amber-400/40 border border-amber-300 rounded-xl">-->
+<!--      Войти-->
+<!--    </button>-->
+<!--  </form>-->
+<!--</main>-->
+
+<style>
+    .bg-grid-slate-400 {
+        background-image: url("./lib/angle.svg");
+    }
+
+    .mask-image {
+        mask-image: linear-gradient(black, transparent);
+    }
+</style>
---
+++ src/lib/ShortLinkCard.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  interface Link {
+    id: string
+    owner: string
+    shortLink: string
+    link: string
+    createdAt: Date
+    updatedAt: Date
+    endAt: Date
+  }
+
+  export let id = '',
+      shortLink = '',
+      link = '',
+      createdAt = null,
+      updatedAt = null,
+      endAt = null,
+      qrcode = '';
+
+  const url = `http://api.${window.location.host}/${shortLink}`
+</script>
+
+<article {id}
+         class="backdrop-blur-2xl group duration-200 text-lg grid shadow-xl py-6 px-8 gap-3 bg-white/40 dark:bg-neutral-700/40 border hover:border-amber-400 border-neutral-50 dark:border-neutral-600 rounded-xl">
+  <button
+      on:click={(event) => {
+        navigator.clipboard.writeText(url).then(() => {
+          alert('Скопировано')
+        });
+      }}
+      class="outline-none duration-200 invisible opacity-0 group-hover:opacity-100 group-hover:visible text-neutral-900 px-4 py-2 group-hover:bg-amber-400/80 group-hover:shadow-md group-hover:hover:shadow-amber-400 focus:ring focus:ring-amber-400/40 border border-transparent group-hover:border-amber-300 rounded-xl">
+    Copy
+  </button>
+  <p>
+    <a href={url} title="Сокращенная ссылка">{url}</a>
+    <span class="block text-sm text-neutral-400 dark:text-neutral-500">shortLink</span>
+  </p>
+  <p>
+    <a href={link} title="Оригинальная ссылка">{link}</a>
+    <span class="block text-sm text-neutral-400 dark:text-neutral-500">link</span>
+  </p>
+  <p>
+    <time title="Создан от">{createdAt.toLocaleString()}</time>
+    <span class="block text-sm text-neutral-400 dark:text-neutral-500">createdAt</span>
+  </p>
+  <p>
+    <time title="Обновлён от">{updatedAt.toLocaleString()}</time>
+    <span class="block text-sm text-neutral-400 dark:text-neutral-500">updatedAt</span>
+  </p>
+  <p>
+    <time title="Закончится">{endAt.toLocaleString()}</time>
+    <span class="block text-sm text-neutral-400 dark:text-neutral-500">endAt</span>
+  </p>
+  <img src={qrcode} alt="qrcode" class="rounded-xl bg-white w-full border border-neutral-50">
+</article>
